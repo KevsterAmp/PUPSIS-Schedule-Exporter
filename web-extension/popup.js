c2863073f1b3461facdd0034bc0d64b9ee84d5a5
KevsterAmp <KevsterAmp@users.noreply.github.com>
2023-09-02 07:28:38 +0800

    initial commit

--- web-extension/popup.js
+++ web-extension/popup.js
@@ -1,17 +1,18 @@
 document.querySelector('#convertButton').addEventListener('click', function() {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     const currentTab = tabs[0];
+    chrome.tabs.sendMessage(currentTab.id, { type: 'getschedule' });
     
-    if ((/sis.*\/student\/schedule$/).test(currentTab.url)) {
-      chrome.tabs.sendMessage(currentTab.id, { type: 'getschedule' });
-    } 
+    // if ((/sis.*\/student\/schedule$/).test(currentTab.url)) {
+    //   chrome.tabs.sendMessage(currentTab.id, { type: 'getschedule' });
+    // } 
     
-    else {
-      // navigates to sis portal
-      chrome.tabs.create({ url: 'https://sis2.pup.edu.ph/student/schedule' }, function(newTab) {
-      chrome.tabs.update(newTab.id, { active: true });
-      });
-    }
+    // else {
+    //   // navigates to sis portal
+    //   chrome.tabs.create({ url: 'https://sis2.pup.edu.ph/student/schedule' }, function(newTab) {
+    //   chrome.tabs.update(newTab.id, { active: true });
+    //   });
+    // }
   });
 });
 
